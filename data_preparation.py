9b142ba16263f2459f75d0f58352cba1b2fc846b
LynnChan706 <512925430@qq.com>
2019-01-11 12:22:52 +0800

    Add the net and data_preparation files

---
+++ data_preparation.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3.5
+# coding=utf-8
+
+'''
+@date = '17/12/1'
+@author = 'lynnchan'
+@email = 'ccchen706@126.com'
+'''
+
+from __future__ import print_function
+
+import os
+import numpy as np
+
+from skimage.io import imsave, imread
+
+data_path = 'data/'
+
+image_rows = 512
+image_cols = 512
+
+
+def create_train_data():
+    train_data_path = os.path.join(data_path, 'train/Image')
+    train_data_Label_path = os.path.join(data_path, 'train/Label')
+    images = os.listdir(train_data_path)
+    total = len(images)
+
+    imgs = np.ndarray((total, image_rows, image_cols), dtype=np.uint8)
+    imgs_mask = np.ndarray((total, image_rows, image_cols), dtype=np.uint8)
+
+    i = 0
+    print('-'*30)
+    print('Creating training images...')
+    print('-'*30)
+    for image_name in images:
+        img = imread(os.path.join(train_data_path, image_name), as_grey=True)
+        img_mask = imread(os.path.join(train_data_Label_path, image_name), as_grey=True)
+
+        img = np.array([img])
+        img_mask = np.array([img_mask])
+
+        imgs[i] = img
+        imgs_mask[i] = img_mask
+
+        if i % 100 == 0:
+            print('Done: {0}/{1} images'.format(i, total))
+        i += 1
+    print('Loading done.')
+
+    np.save('imgs_train.npy', imgs)
+    np.save('imgs_mask_train.npy', imgs_mask)
+    print('Saving to .npy files done.')
+
+
+def load_train_data():
+    imgs_train = np.load('imgs_train.npy')
+    imgs_mask_train = np.load('imgs_mask_train.npy')
+    return imgs_train, imgs_mask_train
+
+
+def create_test_data():
+    train_data_path = os.path.join(data_path, 'test/Image')
+    images = os.listdir(train_data_path)
+    total = len(images)
+
+    imgs = np.ndarray((total, image_rows, image_cols), dtype=np.uint8)
+    imgs_id = np.ndarray((total, ), dtype=np.int32)
+
+    i = 0
+    print('-'*30)
+    print('Creating test images...')
+    print('-'*30)
+    for image_name in images:
+        img_id = int(image_name.split('.')[0])
+        img = imread(os.path.join(train_data_path, image_name), as_grey=True)
+
+        img = np.array([img])
+
+        imgs[i] = img
+        imgs_id[i] = img_id
+
+        if i % 100 == 0:
+            print('Done: {0}/{1} images'.format(i, total))
+        i += 1
+    print('Loading done.')
+
+    np.save('imgs_test.npy', imgs)
+    np.save('imgs_id_test.npy', imgs_id)
+    print('Saving to .npy files done.')
+
+def load_test_data():
+    imgs_test = np.load('imgs_test.npy')
+    imgs_id = np.load('imgs_id_test.npy')
+    return imgs_test, imgs_id
+
+if __name__ == '__main__':
+    create_train_data()
+    create_test_data()
